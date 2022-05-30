db1ebe80c4e6ae1076d9a3088ec9b7e75c9f9375
Gizem Haspolat <gizemhaspolat@gmail.com>
2022-05-30 22:07:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Add your code here
+// fetch("http://localhost:3000/dogs", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+//   body: JSON.stringify({
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   }),
+// })
+//   .then((res) => res.json())
+//   .then((object) => console.log(object))
+//   .catch(function (error) {
+//     alert("Bad things! Ragnarők!");
+//     console.log(error.message);
+//   });
+
+function submitData(name, email) {
+  const userInfo = {
+    name: name,
+    email: email,
+  };
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(userInfo),
+  })
+    .then((res) => res.json())
+    .then((data) => (document.body.innerHTML = data.id))
+    .catch(function (error) {
+      document.body.innerHTML = error.message;
+    });
+}
+submitData();
